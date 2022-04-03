83d6853d2e7fa38a8d685a4cc241a44129f48620
Kaustav-04 <102906116+Kaustav-04@users.noreply.github.com>
2022-04-03 17:57:02 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host="localhost";
-$user="root";
-$password="";
-$db="demo";
-
-$db = new PDO('mysql:host-localhost;dbname='.$db.';charset-utf8',$user,$password);
-$db->setAttribute(PDO::AFTER_ERRMODE, PDO::ERRMODE_EXCEPTION);
